d1687156a34bb35d224b5696b03096174e282f8a
Jordan Hamilton <54907540+inbound-dev@users.noreply.github.com>
2023-01-20 02:08:20 -0500

    more progress

--- scripts/toggleButton.js
+++ scripts/toggleButton.js
@@ -1,9 +1,33 @@
+//background
+const backgroundText = new Array(
+  document.getElementById("listItem1"),
+  document.getElementById("listItem2"),
+  document.getElementById("listItem3"),
+  document.getElementById("header-title"),
+  document.getElementById("header-text")
+);
+
+const background = document.getElementById("header");
+
+var items = backgroundText.length - 1;
+
+//light
 const button = document.getElementById("toggleBtn");
 const light = document.getElementById("light");
 
 function toggleBtn() {
+  // activates the light and changes button color
   button.classList.toggle("active");
   light.classList.toggle("on");
 
-  console.log("we are here!");
+  //one at a time set text item(s) color to black
+  for (let i = 0; i <= items; i++) {
+    backgroundText[i].classList.toggle("activeTextBackground");
+  }
+
+  //change the background white
+
+  background.classList.toggle("activeBackgroundColor");
+
+  console.log("here");
 }
